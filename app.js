133f0d6ef846453669a202d5400ae50c7ce83fe3
viditkhemka63 <viditkhemka63@gmail.com>
2019-05-27 23:06:23 +0530

    writing graphQl schema

--- app.js
+++ app.js
@@ -21,13 +21,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const events = [];
+
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput{
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
     schema {
       query:RootQuery
@@ -36,11 +53,18 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['All night coding ', 'cooking', 'debugging'];
+      return events;
     },
     createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;    
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date().toISOString()
+        }
+        events.push(event); 
+        return event; 
     } 
   },
   graphiql: true
